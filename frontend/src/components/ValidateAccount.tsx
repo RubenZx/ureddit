0cc7b352778be183880820ddce0be3f6be000fe9
RubenZx <ruben.mondom@gmail.com>
2020-05-15 21:48:30 +0200

    :zap: added field on validation page with code

--- frontend/src/components/ValidateAccount.tsx
+++ frontend/src/components/ValidateAccount.tsx
@@ -1,53 +1,83 @@
-import { Card, Col, Row, Spacer, Text } from '@zeit-ui/react'
+import {
+  Card,
+  Col,
+  Input,
+  Link,
+  Row,
+  Spacer,
+  Text,
+  useToasts,
+} from '@zeit-ui/react'
 import { Mail } from '@zeit-ui/react-icons'
-import React, { useEffect, useState } from 'react'
+import { NormalTypes } from '@zeit-ui/react/dist/utils/prop-types'
+import React from 'react'
+import { Controller, useForm } from 'react-hook-form'
 import { useParams } from 'react-router'
 import { validateAccount } from '../services/api'
+import MyButton from './MyButton'
 
 export default () => {
-  const [validate, setValidate] = useState<string>()
   const { validationCode } = useParams()
+  const { control, handleSubmit } = useForm()
 
-  useEffect(() => {
-    if (validationCode) {
-      ;(async () => {
-        try {
-          const res = await validateAccount(validationCode)
-          setValidate(res)
-        } catch (error) {
-          setValidate(error.response.data.messages.error)
-        }
-      })()
+  const [, setToast] = useToasts()
+  const openToast = (type: NormalTypes, text: string) =>
+    setToast({
+      text,
+      type,
+    })
+
+  const onSubmit = async (value: Record<string, string>) => {
+    try {
+      const res = await validateAccount(value.verificationCode)
+      openToast('success', res)
+    } catch (error) {
+      openToast(
+        'error',
+        `${error.response.status}: ${error.response.data.messages.error}`,
+      )
     }
-  }, [validationCode, setValidate])
+  }
 
   return (
     <Row justify="center" align="middle" style={{ minHeight: '70vh' }}>
-      <Card shadow style={{ maxWidth: '420px' }}>
+      <Card shadow style={{ maxWidth: '280pt' }}>
         <Row justify="space-around">
           <Col>
-            <Row justify="center">
-              {validationCode ? (
-                <Text h3>Validating your account...</Text>
-              ) : (
-                <Text h3>Please, confirm your account</Text>
-              )}
-            </Row>
-            <Row justify="center">
-              <Text h4>
-                <Row align="middle">
-                  {validationCode ? (
-                    validate
-                  ) : (
-                    <>
-                      Check your email inbox
-                      <Spacer x={0.5} />
-                      <Mail />
-                    </>
-                  )}
-                </Row>
-              </Text>
-            </Row>
+            <Text h3>Please, confirm your account</Text>
+            <Text h4>
+              <Row align="middle">
+                Check your email inbox
+                <Spacer x={0.5} />
+                <Mail />
+              </Row>
+            </Text>
+            <form
+              onSubmit={(e) => {
+                handleSubmit(onSubmit)(e)
+              }}
+            >
+              Validation code
+              <Controller
+                as={Input}
+                value={validationCode}
+                name="verificationCode"
+                width="100%"
+                control={control}
+                defaultValue={validationCode}
+              />
+              <Row>
+                <Text small>
+                  Didn't receive anything? <Link color>Resend email</Link>
+                </Text>
+              </Row>
+              <Spacer />
+              <Row justify="end">
+                <MyButton type="success" size="small" shadow>
+                  Validate account
+                </MyButton>
+              </Row>
+            </form>
           </Col>
         </Row>
       </Card>
